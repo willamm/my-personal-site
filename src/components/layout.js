0749161d4ea9845de18ee4cbd0def9d3448031ea
William Murphy <will.murphy1994@gmail.com>
2023-02-02 14:59:36 -0800

    Add Google Docs link to published resume

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,13 +3,14 @@ import { Link } from 'gatsby'
 import { container, heading, navLinks, navLinkItem, navLinkText } from './layout.module.css'
 
 const Layout = ({pageTitle, children}) => {
+    const resumeUrl = "https://docs.google.com/document/d/e/2PACX-1vRg9ZTIo6cTKuXx342YACpauM68GIN2EMEphhUc3xUKnzZw8VGZAuokDOfp61BAWRaSHLs9cKUYyENr/pub"
     return (
         <div className={container}>
             <nav>
                 <ul className={navLinks}>
                     <li className={navLinkItem}><Link className={navLinkText} to="/">Home</Link></li>
                     <li className={navLinkItem}><Link className={navLinkText} to="/about">About</Link></li>
-                    <li className={navLinkItem}><Link className={navLinkText} to="/images/resume.pdf">Resume</Link></li>
+                    <li className={navLinkItem}><Link className={navLinkText} to={resumeUrl}>Resume</Link></li>
                 </ul>
             </nav>
             <main>
