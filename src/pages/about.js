1a0b55121dee21f1595f435e2e7fd52bc456ffe7
William Murphy <will.murphy1994@gmail.com>
2023-02-12 11:59:57 -0800

    revert

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import HitCounter from '../components/HitCounter';
 import Layout from '../components/layout'
 import useSiteMetadata from '../hooks/useSiteMetadata';
-import JSONbuildtime from './json-at-buildtime';
 
 
 export const Head = () => <title>About Me</title>
@@ -11,8 +10,6 @@ const AboutPage = ({location}) => {
     return (
         <Layout pageTitle="About">
             <p>This is the About Me page</p>
-            <JSONbuildtime></JSONbuildtime>
-            <JSONbuildtime></JSONbuildtime> 
             <HitCounter slug={location.pathname}></HitCounter>
         </Layout>
     )
