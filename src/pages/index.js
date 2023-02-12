f1556fae0fe4e7913a306aa2b23af30c732ff62f
William Murphy <will.murphy1994@gmail.com>
2023-02-12 09:11:53 -0800

    Remove unused imports in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,7 @@
 import { graphql, Link, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
 import * as React from "react"
-import { useEffect, useState } from "react"
-import { useCallback } from "react"
 import HitCounter from "../components/HitCounter"
-import {heading} from "../components/layout.module.css"
 import JSONbuildtime from "./json-at-buildtime"
 
 const IndexPage = ({location}) => {
