96e74283b4a1e2a6a34b0cec219d11da768c101a
William Murphy <will.murphy1994@gmail.com>
2023-02-12 11:13:35 -0800

    Add blog post title and links to Blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,32 @@
+import { graphql } from 'gatsby';
 import * as React from 'react';
 import HitCounter from '../components/HitCounter';
 import Layout from '../components/layout';
+import { Link } from 'gatsby';
 
 export const Head = () => <title>Blog</title>
 
-const BlogPage = ({location}) => {
+export const query = graphql`
+    query BlogPostSlugs {
+        allMdx {
+            nodes {
+                frontmatter {
+                    slug
+                    title
+                }
+            }
+        }
+    }
+`
+const BlogPage = ({data, location}) => {
     return (
         <Layout pageTitle="Blog">
             <p>This is the blog page. Blog posts go here.</p>
+            <div>
+                {data.allMdx.nodes.map((post) => {
+                    return <li key={post.id}><Link to={post.frontmatter.slug}>{post.frontmatter.title}</Link></li>
+                })}
+            </div>
             <HitCounter slug={location.pathname} />
         </Layout>
     )
